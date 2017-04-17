99b447308def373bbfe7ff3ca6e38386090ea35b
Matan Bendix Shenhav <matan@fluxcraft.net>
2017-04-17 19:48:02 +0300

    Finally fixed packaging.

--- fluxscript.nix
+++ fluxscript.nix
@@ -1,4 +1,4 @@
-{ pkgs, stdenv}:
+{ stdenv }:
 
 let
 
@@ -12,14 +12,8 @@ rec
 
     src = ./.;
 
-    
-
-    phases = [ "unpackPhase" "installPhase" ];
-
-
     installPhase =
         ''
-            set -x
             mkdir -p $out
             cp -R ./bin $out/bin
             cp -R ./lib $out/lib
